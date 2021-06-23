c8d3399d9e14277cfb4d8fa2d376d66f1a560486
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-06-23 13:41:17 -0300

    fix: Composer script

--- composer.sh
+++ composer.sh
@@ -7,13 +7,22 @@ if ! docker info > /dev/null 2>&1; then
 	exit 1
 fi
 
-COMPOSER_HOME=/tmp
+USERID=$(id -u)
+GROUPID=$(id -g)
+COMPOSER_HOME=/tmp/composer
+COMPOSER_CACHE_DIR=/.composer
 
 # Run command
 docker run --rm --interactive --tty \
-  --env COMPOSER_HOME \
-  --volume ${COMPOSER_HOME:-$HOME/.config/composer}:$COMPOSER_HOME \
-  --volume $PWD:/app \
-  --user $(id -u):$(id -g) \
-  --workdir /app \
-  composer $1
+	--env COMPOSER_HOME \
+	--env COMPOSER_CACHE_DIR \
+	--volume $HOME/.config/composer:$COMPOSER_HOME \
+	--volume $HOME/.cache/composer:$COMPOSER_CACHE_DIR \
+	--volume $PWD:/app \
+	--workdir /app \
+	composer:latest \
+	/bin/sh -c "addgroup -g $GROUPID -S hostusr ; \
+		adduser -u $USERID -s /bin/bash -D -S -G hostusr hostusr ; \
+	 	chown -R hostusr:hostusr $COMPOSER_HOME ; \
+	 	chown -R hostusr:hostusr $COMPOSER_CACHE_DIR ; \
+		su - hostusr -c 'PATH=\$PATH:/usr/local/bin && cd /app && composer $1'"
